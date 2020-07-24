8e38bbc801a046c54956cbd085389a51be341ea1
Cesar Diaz <cesdia883@gmail.com>
2020-07-24 17:39:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function whisper(string){
   return string.toLowerCase()
 }
 
+function logShout(string){
+  return console.log(shout(string))
+}
+
+function logWhisper(string){
+  return console.log(whisper(string))
+}
+
 function sayHiToGrandma(string){
   if (whisper(string) === string){
     return "I can't hear you!"
