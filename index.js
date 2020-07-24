de70eb88ed2410a042b2a7bfbe2e99420dd2ffd5
Cesar Diaz <cesdia883@gmail.com>
2020-07-24 17:37:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   } else if (shout(string) === string){
     return shout("yes indeed!")
-  } else if (string === "I love you, too."){
-    return "I love you, Grandma."
+  } else if (string === "I love you, Grandma."){
+    return "I love you, too."
   }
 }
